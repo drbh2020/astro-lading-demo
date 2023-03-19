2d333fdea845fd105b2064e4bb055bcea6aff18b
Diego Berastain <berastaindiego@gmail.com>
2023-03-19 17:27:35 -0500

    astro landing v1

---
+++ src/components/landing/index.js
@@ -0,0 +1,10 @@
+/* Si funciona*/
+// import Hero from './Hero.astro';
+// import More from "./More.astro";
+// import Features from "./Features.astro";
+// import About from "./About.astro";
+
+
+// export {
+//     Hero, More, Features, About
+// } 
